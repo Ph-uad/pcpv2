fe5c25a90fa64919483421e10e80ed1dc053a965
Ph-uad <temidayo26j@gmail.com>
2025-01-27 17:11:05 +0000

    emergency back up 1

--- DATA_WRANGLER.py
+++ DATA_WRANGLER.py
@@ -5,34 +5,34 @@ import pandas as pd
 class DATA_WRANGLER:
     def __init__(self, file_path):
         self.file_path = file_path
-        
+
     def load_data(self):
         # Load data from the specified file path and if file path does not exist raise FileNotFoundError
         if not os.path.exists(self.file_path):
             raise FileNotFoundError(f"File not found: {self.file_path}")
-        #else load the data from the specified file path
+        # else load the data from the specified file path
         return pd.read_csv(self.file_path)
-    
-    def detect_mixed_columns(self):
+
+    def detect_mixed_columns(self, df):
         mixed_columns = []
         # Check for mixed type columns in the data and store them in the mixed_columns list
-        for col in self.data.columns:
-            types = self.data[col].map(type).unique()
-            #append types to the mixed_columns list if the length of types is greater than 1
-            for type in types:
+        for col in df.columns:
+            types = df[col].map(type).unique()
+            # Append the column name to mixed_columns if more than one type is detected
+            if len(types) > 1:
                 mixed_columns.append((col, types))
         return mixed_columns
-    
-    def coerce_mixed_columns(self):
+
+    def coerce_mixed_columns(self, df):
         mixed_columns = []
         # Coerce the mixed type columns to numeric and drop the rows with missing values
-        for column in self.detect_mixed_columns():
+        for column in self.detect_mixed_columns(df):
             mixed_columns.append(column[0])
         for column in mixed_columns:
-            self.data[column] = pd.to_numeric(self.data[column], errors='coerce')
-        self.data.dropna(inplace=True)
-        return self.data
-    
+            df[column] = pd.to_numeric(df[column], errors='coerce')
+        df.dropna(inplace=True)
+        return df
+
     def load_and_clean_data(self):
-        self.data = self.load_data()
-        return self.coerce_mixed_columns()
+        df = self.load_data()
+        return self.coerce_mixed_columns(df)
--- RANKING.py
+++ RANKING.py
@@ -1,43 +1,66 @@
-class RANKING:
-    def __init__(self, df, name_column, exclude_features = ["artists","id","name","loudness","release_date"]):
+import random
+
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.sparse import csr_matrix
+from sklearn.preprocessing import normalize
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import csr_matrix
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+
+
+class Recommendation_Module:
+    # "artists", "id", "name", "loudness", "release_date"
+    def __init__(self, df, name_column):
         self.df = df
         # Name of the column that contains the artist names in our case "artists"
         self.name_column = name_column
         self.exclude_features = exclude_features
         self.artist = df[name_column].tolist()
-        
+
         # self.target_column = target_column
         
-        
+    def extract_features(self):
+        if "genres" not in  self.df.columns:
+            return self.df.drop(colums = ["artists", "id", "name", "release_date"])
+        else:
+            return self.df.drop(columns = ["id"])
+
     def get_random_artist_observation_index_by_name(self, name):
         indexes = []
-        
+
         # find all indexes of artists that contain the name
         for x in range(len(self.artist)):
             if name.lower() in str(self.artist[x]).lower():
                 indexes.append(x)
-        
+
         # if no matches found, return None
         if len(indexes) == 0:
             return None
         else:
-            # return a random index from the found indexes 
-            return  indexes[random.randint(0, len(indexes))]
-        
-        
-    def get_cosine_similarity_matrix(self, name, n = 5):
-        
-        target_index = self.get_random_artist_observation_index_by_name(name)
-        
-        if target_index is None:
-            return None
-        
+            # return a random index from the found indexes
+            return indexes[random.randint(0, len(indexes))]
+
+    def normalize_data(self):
         if self.exclude_features is not None:
-          data = self.df.drop(columns=self.exclude_features).values
+            data = self.df.drop(columns=self.exclude_features).values
         else:
             data = self.df.values
-        
-        normalized_data = normalize(data)
+
+        return normalize(data)
+
+    def get_top_n_similar_variables(self, name, n=5):
+
+        target_index = self.get_random_artist_observation_index_by_name(name)
+
+        if target_index is None:
+            return None
+
+        normalized_data = normalized_data()
+
         sparse_data = csr_matrix(normalized_data)
         similarity_scores = cosine_similarity(sparse_data, dense_output=False)
         target_similarity = similarity_scores[target_index].toarray().flatten()
@@ -45,88 +68,49 @@ class RANKING:
         top_n_indices = np.argsort(target_similarity)[-n:][::-1]
 
         return top_n_indices
-    
-    
 
+    def generate_recommendation_data(self, name, n=5):
+        scaler = StandardScaler()
+        
+        normalized_data = self.normalize_data()
+        scaled_features = scaler.fit_transform(normalized_data)
 
-# def get_cosine_similarity_matrix(df, target_index, exclude_features = None, n = 5):
-    
-#     if exclude_features is not None:
-#        data = df.drop(columns=exclude_features).values
-#     else:
-#         data = df.values
-    
-#     similarity_scores = cosine_similarity(data)
-    
-#     target_similarity = similarity_scores[target_index]
-    
-#     target_similarity[target_index] = -1
-    
-#     top_n_indices = np.argsort(target_similarity)[-n:][::-1]
-    
-#     return top_n_indices
+        # Apply K-Means
+        kmeans = KMeans(n_clusters=3, random_state=100)
+        self.df['Cluster'] = kmeans.fit_predict(scaled_features)
 
+        # Recommend items from the same cluster
+        target_cluster = 0
+        recommendations = self.df[self.df['Cluster'] == self.df[self.df[self.name_column] == name]['Cluster']].head(n)
+        
+        recommendations = self.df[self.df['Cluster'] == target_cluster]
+        print("Items in Cluster 0 for Recommendation:")
+        print(recommendations)
+ 
+        return recommendations
 
-
-# import numpy as np
-# from sklearn.metrics.pairwise import cosine_similarity
-
-# def get_cosine_similarity_matrix(df, target_index, exclude_features = None, n = 5):
-    
-#     if exclude_features is not None:
-#        data = df.drop(columns=exclude_features).values
-#     else:
-#         data = df.values
-    
-#     similarity_scores = cosine_similarity(data)
-    
-#     target_similarity = similarity_scores[target_index]
-    
-#     target_similarity[target_index] = -1
-    
-#     top_n_indices = np.argsort(target_similarity)[-n:][::-1]
-    
-#     return top_n_indices
-    
-#for genres
-# get_cosine_similarity_matrix(pd, 0, 10, ["genres", "duration_ms"])
-
-# for artist
-# get_cosine_similarity_matrix(pd, 2, ["artists","id","name","loudness","release_date"])
-
-# def get_random_artist_observation_index_by_name(name, artist):
-#     indexes = []
-    
-#     # find all indexes of artists that contain the name
-#     for x in range(len(artist)):
-#         if name.lower() in str(artist[x]).lower():
-#             indexes.append(x)
-    
-#     # if no matches found, return None
-#     if len(indexes) == 0:
-#         return None
-#     else:
-#         # return a random index from the found indexes
-#         return  indexes[random.randint(0, len(indexes))]
-    
-    
-
-# get_random_artist_observation_index_by_name('Mehmet')
-
-# def get_cosine_similarity_matrix(df, target_index, exclude_features = None, n = 5):
-    
-#     if exclude_features is not None:
-    #    data = df.drop(columns=exclude_features).values
-#     else:
-#         data = df.values
-    
-#     similarity_scores = cosine_similarity(data)
-    
-#     target_similarity = similarity_scores[target_index]
-    
-#     target_similarity[target_index] = -1
-    
-#     top_n_indices = np.argsort(target_similarity)[-n:][::-1]
-    
-#     return top_n_indices
-    
+        
+    def optimize_k_means(self, max_clusters=10):      
+        means_values = []
+        inertia_values = []
+        
+        for k in range(1, max_clusters):
+            kmeans = KMeans(n_clusters=k)
+            kmeans.fit(self.normalize_data())
+            
+            means_values.append(k)
+            inertia_values.append(kmeans.inertia_)
+            
+        fig =plt.subplots(figsize=(10, 6))
+        plt.plot(means_values, inertia_values, 'o-')
+        plt.xlabel('Number of clusters')
+        plt.ylabel('Inertia')
+        plt.title('Elbow Method For Optimal k')
+        plt.grid(True)
+        plt.show()
+        
+        
+        
+        
+        
+#https://youtu.be/iNlZ3IU5Ffw?si=TK5_aHZcTlo7C7i4
